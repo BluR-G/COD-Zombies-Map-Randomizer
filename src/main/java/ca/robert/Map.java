9e98633fe44fc676550b2d6b0844485ec573992b
Robert_G <shadow-of-darknes@live.ca>
2023-02-02 10:26:10 -0500

    add / tested implementation of Map image source

--- src/main/java/ca/robert/Map.java
+++ src/main/java/ca/robert/Map.java
@@ -1,5 +1,7 @@
 package ca.robert;
 
+import java.io.File;
+
 import ca.robert.CallOfDuty.Game;
 
 public class Map {
@@ -7,11 +9,24 @@ public class Map {
   private String imgSrc;
   private Game game;
 
-  
   public Map(String map, Game game, String imgSrc) {
     this.map = map;
     this.game = game;
-    this.imgSrc = imgSrc;
+    String validatedImgStr = doesImageExist(imgSrc);
+    this.imgSrc = validatedImgStr;
+  }
+
+  private String doesImageExist(String imgSrc) {
+    File mapFile = new File(imgSrc);
+    if (!mapFile.exists() || mapFile.isDirectory()) {
+      String defaultImageSrc = "src/main/resources/ca/robert/img/UI/default_map_image.jpg";
+      File defaultImageFile = new File(defaultImageSrc);
+      if (!defaultImageFile.exists() || defaultImageFile.isDirectory()) {
+        throw new IllegalArgumentException("File doesn't exist");
+      }
+      imgSrc = defaultImageSrc;
+    }
+    return imgSrc;
   }
 
   public String getMap() {
@@ -25,10 +40,9 @@ public class Map {
   public String getImgSrc() {
     return imgSrc;
   }
-  
 
   public String getGameStr() {
-    switch(game){
+    switch (game) {
       case WORLD_AT_WAR:
         return "World at War";
       case BLACK_OPS_1:
--- src/test/java/ca/robert/MapTest.java
+++ src/test/java/ca/robert/MapTest.java
@@ -35,5 +35,26 @@ public class MapTest {
         String actualGame = map.getGameStr();
         assertNotEquals(expectedGame, actualGame);
     }
+
+    @Test
+    public void testGetImgSrcFoundImage() {
+        String expectedImgSrc = "src/main/resources/ca/robert/img/WAW/Nacht_Der_Untoten_WaW.png";
+        Map map = new Map("test", Game.WORLD_AT_WAR, expectedImgSrc);
+        assertEquals(expectedImgSrc, map.getImgSrc());
+    }
+
+    @Test
+    public void testGetImgSrcDefaultImage1() {
+        String expectedImgSrc = "src/main/resources/ca/robert/img/UI/default_map_image.jpg";
+        Map map = new Map("test", Game.WORLD_AT_WAR, "test");
+        assertEquals(expectedImgSrc, map.getImgSrc());
+    }
+
+    @Test
+    public void testGetImgSrcDefaultImage2() {
+        String expectedImgSrc = "src/main/resources/ca/robert/img/UI/default_map_image.jpg";
+        Map map = new Map("test", Game.WORLD_AT_WAR, "src/main/resources/ca/robert/img/WAW/");
+        assertEquals(expectedImgSrc, map.getImgSrc());
+    }
     
 }
